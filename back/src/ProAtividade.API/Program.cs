c04f298c07a8664e4999c6176ff70a42b49fd6d1
Cleomilson Sales <cleomilsonsales@hotmail.com>
2023-10-04 21:10:21 -0300

    estudos de 04/10/2023 21:10
    Atualizando .net de 5.0 para 6.0

--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,12 +1,59 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+//using enviada para o GlobalUsings.cs
 
+/* versão .net 6.0 */
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+);
+
+//carregando as interfaces no contexto
+builder.Services.AddScoped<IAtividadeRepo, AtividadeRepo>();
+builder.Services.AddScoped<IGeralRepo, GeralRepo>();
+builder.Services.AddScoped<IAtividadeService, AtividadeService>();
+// Add services to the container.
+builder.Services.AddControllers()
+                .AddJsonOptions(options => {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });;
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProAtividade.API", Version = "v1" });
+});
+
+//liberando o CORS para o front acessar o back
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProAtividade.API v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//liberando o CORS para o front acessar o back
+//atenção a ordem que esse codigo existe importa
+app.UseCors(option => option.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin());
+
+app.MapControllers();
+
+app.Run();
+
+
+
+/* versão .net 5.0
 namespace ProAtividade.API
 {
     public class Program
@@ -24,3 +71,4 @@ namespace ProAtividade.API
                 });
     }
 }
+*/
